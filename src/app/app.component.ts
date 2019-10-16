080d984fe5b6350d88c3bd87931979fec624356b
manigandan63 <manigandant5688@gmail.com>
2019-10-17 00:30:31 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {Router, NavigationStart } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'loginApp';
+  showLogin: boolean = false;
+  showLogout: boolean = false;
+  constructor(private router: Router) {
+    router.events.forEach((event) => {
+      if (event instanceof NavigationStart) {
+        if (event['url'] == '/login') {
+          this.showLogin = false;
+        } else if (event['url'] == '/user') {
+          this.showLogout = true;
+        } else {
+          this.showLogin = true;
+          this.showLogout = false;
+        }
+      }
+    });
+   }
+  LoginBtn = () => {
+    this.router.navigate(["login"]);
+  };
+  LogOutBtn = () => {
+    this.router.navigate(["/"]);
+  };
+
 }
