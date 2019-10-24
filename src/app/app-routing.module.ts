f58ffade8a270a0b72708cb5cf1480fa1fa5349a
Girishr1406 <r.girish6@gmail.com>
2019-10-24 13:09:19 +0530

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { UserComponent } from './user/user.component';
+import { CrudComponent } from './crud/crud.component';
 
 const routes: Routes = [
-  { path: 'user', component: UserComponent },
+  { path: 'crud', component: CrudComponent },
   { path: 'login', component: LoginComponent }
 ];
 
